e427b9d0413e0b4ac822286e05346ce07a98e08c
Quinn Zieltjens <quinnzieltjens@gmail.com>
2023-09-28 13:47:02 +0200

    feat:ScripAdder
    created an abstract class which will add the derived type to the component passed in T

---
+++ Assets/Scripts/ScriptAdder.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+/// <summary>
+/// used to add a itself to the gameObject which contains <see cref="T"/> on <see cref="Awake"/>
+/// </summary>
+public abstract class ScriptAdder<T> : MonoBehaviour where T : MonoBehaviour
+{
+    protected virtual void Awake()
+    {
+        //get the component with the given MonoBehaviour
+        MonoBehaviour monoBehaviour = GetComponent<T>();
+
+        //if the ballMovement is null, it means we are not on the gameObject with the MonoBehaviour
+        if (monoBehaviour != null)
+            return; //return because we are on MonoBehaviour
+
+        //find MonoBehaviour and add the  to it
+        monoBehaviour = FindObjectOfType<T>(); //find the object of type T
+        monoBehaviour.gameObject.AddComponent(GetType()); //GetType() gets the type of the instance
+    }
+}
