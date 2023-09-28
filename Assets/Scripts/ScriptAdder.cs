63362c44553674d20b9c3a800da20e895c11032d
Quinn Zieltjens <quinnzieltjens@gmail.com>
2023-09-28 14:30:10 +0200

    fix:scriptadder_type_param
    - improved documentation
    - changed it so you can use any component rather than just MonoBehaviours

--- Assets/Scripts/ScriptAdder.cs
+++ Assets/Scripts/ScriptAdder.cs
@@ -3,20 +3,21 @@ using UnityEngine;
 /// <summary>
 /// used to add a itself to the gameObject which contains <see cref="T"/> on <see cref="Awake"/>
 /// </summary>
-public abstract class ScriptAdder<T> : MonoBehaviour where T : MonoBehaviour
+/// <typeparam name="T">the component that is on the gameObject an instance type is added to</typeparam>
+public abstract class ScriptAdder<T> : MonoBehaviour where T : Component //make sure the type parameter T is derived from a Component
 {
     protected virtual void Awake()
     {
-        //get the component with the given MonoBehaviour
-        MonoBehaviour monoBehaviour = GetComponent<T>();
+        //get the component with the given component
+        Component component = GetComponent<T>();
 
-        //if the ballMovement is null, it means we are not on the gameObject with the MonoBehaviour
-        if (monoBehaviour != null)
-            return; //return because we are on MonoBehaviour
+        //if the ballMovement is null, it means we are not on the gameObject with the component
+        if (component != null)
+            return; //return because we are on the component
 
-        //find MonoBehaviour and add the  to it
-        monoBehaviour = FindObjectOfType<T>(); //find the object of type T
-        monoBehaviour.gameObject.AddComponent(GetType()); //GetType() gets the type of the instance
-        Destroy(this);
+        //find the component and add the  to it
+        component = FindObjectOfType<T>(); //find the object of type T
+        component.gameObject.AddComponent(GetType()); //GetType() gets the type of the instance
+        Destroy(this); //remove the instance from the current script because we don't belong here >:3
     }
 }
